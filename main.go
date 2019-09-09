4789de7b94ce78ec6ee73fc0fcebef057a52e42c
Fajar Firdaus <35714197+FajarTheGGman@users.noreply.github.com>
2019-09-09 08:14:03 +0000

    Add files via upload

---
+++ main.go
@@ -0,0 +1,39 @@
+/*
+Copyright© 2019 By Fajar Firdaus
+
+Please don't recode my program because i take a long time to complete this project :)
+
+*/
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"io/ioutil"
+)
+
+func main(){
+	fmt.Print("{\n")
+	fmt.Print("  Coder : Fajar Firdaus\n")
+	fmt.Print("  Fb : Ace.of.spades729\n")
+	fmt.Print("  IG : fajar_firdaus_7\n")
+	fmt.Print("  YT : iTech7732\n")
+	fmt.Print("}\n\n")
+
+	files, err := ioutil.ReadDir("./htdocs")
+    if err != nil {
+        fmt.Print("[!] File Empty")
+	}
+	
+	http.Handle("/", http.FileServer(http.Dir("./htdocs")))
+	
+	fmt.Print("[Server Content]\n")
+    for _, f := range files {
+		fmt.Print("- " + f.Name() + "\n")
+	}
+
+	fmt.Print("\n[+] Server Running At Port 3000")
+	http.ListenAndServe(":3000", nil)
+	
+}
